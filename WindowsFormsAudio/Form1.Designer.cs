1cfdebf5d3d2e2bbc24cd835aa15391dc4cc2e22
Ilya <graver9062@tutanota.de>
2021-12-07 21:45:47 +0300

    Add design of project

---
+++ WindowsFormsAudio/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace WindowsFormsAudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BackPanel = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.slidePanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNext = new WindowsFormsAudio.RoundButton();
+            this.btnStop = new WindowsFormsAudio.RoundButton();
+            this.btnPlay = new WindowsFormsAudio.RoundButton();
+            this.btnPause = new WindowsFormsAudio.RoundButton();
+            this.btnPrevious = new WindowsFormsAudio.RoundButton();
+            this.BackPanel.SuspendLayout();
+            this.slidePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BackPanel
+            // 
+            this.BackPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(41)))), ((int)(((byte)(59)))));
+            this.BackPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BackPanel.Controls.Add(this.btnNext);
+            this.BackPanel.Controls.Add(this.btnStop);
+            this.BackPanel.Controls.Add(this.btnPlay);
+            this.BackPanel.Controls.Add(this.btnPause);
+            this.BackPanel.Controls.Add(this.btnPrevious);
+            this.BackPanel.Controls.Add(this.progressBar1);
+            this.BackPanel.Location = new System.Drawing.Point(0, 0);
+            this.BackPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.BackPanel.Name = "BackPanel";
+            this.BackPanel.Size = new System.Drawing.Size(1333, 423);
+            this.BackPanel.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.HotPink;
+            this.progressBar1.Location = new System.Drawing.Point(47, 296);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1114, 10);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // slidePanel
+            // 
+            this.slidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(85)))), ((int)(((byte)(105)))));
+            this.slidePanel.Controls.Add(this.label2);
+            this.slidePanel.Location = new System.Drawing.Point(-360, 0);
+            this.slidePanel.Margin = new System.Windows.Forms.Padding(4);
+            this.slidePanel.Name = "slidePanel";
+            this.slidePanel.Size = new System.Drawing.Size(400, 615);
+            this.slidePanel.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 153);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n";
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNext.BackgroundImage")));
+            this.btnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNext.FlatAppearance.BorderSize = 0;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Location = new System.Drawing.Point(743, 322);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(80, 74);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.MouseEnter += new System.EventHandler(this.btnNext_MouseEnter);
+            this.btnNext.MouseLeave += new System.EventHandler(this.btnNext_MouseLeave);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStop.BackgroundImage")));
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Location = new System.Drawing.Point(657, 322);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(80, 74);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.MouseEnter += new System.EventHandler(this.btnStop_MouseEnter);
+            this.btnStop.MouseLeave += new System.EventHandler(this.btnStop_MouseLeave);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPlay.BackgroundImage")));
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Location = new System.Drawing.Point(571, 322);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(80, 74);
+            this.btnPlay.TabIndex = 9;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.MouseEnter += new System.EventHandler(this.btnPlay_MouseEnter);
+            this.btnPlay.MouseLeave += new System.EventHandler(this.btnPlay_MouseLeave);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPause.BackgroundImage")));
+            this.btnPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPause.FlatAppearance.BorderSize = 0;
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.Location = new System.Drawing.Point(485, 322);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(80, 74);
+            this.btnPause.TabIndex = 8;
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.MouseEnter += new System.EventHandler(this.btnPause_MouseEnter);
+            this.btnPause.MouseLeave += new System.EventHandler(this.btnPause_MouseLeave);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPrevious.BackgroundImage")));
+            this.btnPrevious.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPrevious.FlatAppearance.BorderSize = 0;
+            this.btnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevious.Location = new System.Drawing.Point(399, 322);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(80, 74);
+            this.btnPrevious.TabIndex = 7;
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.MouseEnter += new System.EventHandler(this.btnPrevious_MouseEnter);
+            this.btnPrevious.MouseLeave += new System.EventHandler(this.btnPrevious_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 409);
+            this.Controls.Add(this.slidePanel);
+            this.Controls.Add(this.BackPanel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.BackPanel.ResumeLayout(false);
+            this.slidePanel.ResumeLayout(false);
+            this.slidePanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel BackPanel;
+        private System.Windows.Forms.Panel slidePanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private RoundButton btnPrevious;
+        private RoundButton btnPause;
+        private RoundButton btnNext;
+        private RoundButton btnStop;
+        private RoundButton btnPlay;
+    }
+}
+
---
+++ WindowsFormsAudio/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAudio
+{
+    public partial class Form1 : Form
+    {
+        bool expectation; //  чтобы события не перебивали друг друга
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            slidePanel.MouseEnter += async (s, a) =>  // панель выезжает
+            {
+                while (!expectation && slidePanel.Location.X < BackPanel.Location.X)
+                {
+                    expectation = true;
+                       await Task.Delay(1);
+                    slidePanel.Location = new Point(slidePanel.Location.X - slidePanel.Location.X/8+1, slidePanel.Location.Y);
+                    expectation = false;
+
+                }
+            };
+
+            BackPanel.MouseEnter += async (s, a) =>   // панель уезжает обратно
+            {
+                while (!expectation && slidePanel.Location.X > -270)
+                {
+                    expectation = true;
+                    await Task.Delay(1);
+                    slidePanel.Location = new Point(slidePanel.Location.X - (slidePanel.Width + slidePanel.Location.X)/8+1, slidePanel.Location.Y);
+                    expectation = false; 
+
+                }
+            };
+        }
+
+        private void btnPrevious_MouseEnter(object sender, EventArgs e)
+        {
+            btnPrevious.BackColor = Color.FromArgb(244, 63, 94);
+        }
+
+        private void btnPrevious_MouseLeave(object sender, EventArgs e)
+        {
+            btnPrevious.BackColor = Color.FromArgb(30, 41, 59);
+        }
+
+        private void btnPause_MouseEnter(object sender, EventArgs e)
+        {
+            btnPause.BackColor = Color.FromArgb(244, 63, 94);
+        }
+
+        private void btnPause_MouseLeave(object sender, EventArgs e)
+        {
+            btnPause.BackColor = Color.FromArgb(30, 41, 59);
+        }
+
+        private void btnPlay_MouseEnter(object sender, EventArgs e)
+        {
+            btnPlay.BackColor = Color.FromArgb(244, 63, 94);
+        }
+
+        private void btnPlay_MouseLeave(object sender, EventArgs e)
+        {
+            btnPlay.BackColor = Color.FromArgb(30, 41, 59);
+        }
+
+        private void btnStop_MouseEnter(object sender, EventArgs e)
+        {
+            btnStop.BackColor = Color.FromArgb(244, 63, 94);
+        }
+
+        private void btnStop_MouseLeave(object sender, EventArgs e)
+        {
+            btnStop.BackColor = Color.FromArgb(30, 41, 59);
+        }
+
+        private void btnNext_MouseEnter(object sender, EventArgs e)
+        {
+            btnNext.BackColor = Color.FromArgb(244, 63, 94);
+        }
+
+        private void btnNext_MouseLeave(object sender, EventArgs e)
+        {
+            btnNext.BackColor = Color.FromArgb(30, 41, 59);
+        }
+    }
+}
